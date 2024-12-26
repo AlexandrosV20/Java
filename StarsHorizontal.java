b76d0f34824929760b696e53bb46e80bb8be84a2
Αλέξανδρος Βυθούλκας <alexandrosvithoulkas@gmail.com>
2024-12-26 18:06:32 +0200

    Add files via upload

---
+++ StarsHorizontal.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch4;
+
+import java.util.Scanner;
+
+public class StarsHorizontal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of stars (n): ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println(); // Για να περάσει σε νέα γραμμή
+        sc.close();
+    }
+}
